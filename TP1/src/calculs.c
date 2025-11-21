b132f2f2e58a3a046b5d583bfa2ad8412350955c
Nadir <94380694+xboxmomo23@users.noreply.github.com>
2025-11-21 15:08:34 +0100

    Implement basic calculator with multiple operations

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,66 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+
+    printf("Entrez un operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);  // espace avant %c pour consommer les retours à la ligne
+
+    switch (op) {
+
+        case '+':
+            printf("Resultat : %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Resultat : %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Resultat : %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Erreur : division par zero !\n");
+            } else {
+                printf("Resultat : %d\n", num1 / num2);
+            }
+            break;
+
+        case '%':
+            if (num2 == 0) {
+                printf("Erreur : modulo par zero !\n");
+            } else {
+                printf("Resultat : %d\n", num1 % num2);
+            }
+            break;
+
+        case '&':
+            printf("Resultat (ET binaire) : %d\n", num1 & num2);
+            break;
+
+        case '|':
+            printf("Resultat (OU binaire) : %d\n", num1 | num2);
+            break;
+
+        case '~':
+            printf("Resultat (~num1) : %d\n", ~num1);
+            printf("Resultat (~num2) : %d\n", ~num2);
+            break;
+
+        default:
+            printf("Erreur : operateur inconnu.\n");
+    }
+
+    return 0;
+}
+
 
