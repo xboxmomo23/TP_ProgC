b639325cebe75c901f7474f5b570d237ac1f54f6
Nadir <94380694+xboxmomo23@users.noreply.github.com>
2025-11-21 15:13:25 +0100

    Add pyramid generation program in pyramide.c
    This program generates a pyramid pattern using numbers.

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,29 @@
+ #include <stdio.h>
 
+int main() {
+    int n = 5; // Hauteur de la pyramide
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        // Afficher les espaces pour centrer la pyramide
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Afficher les nombres croissants de 1 à i
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Afficher les nombres décroissants de i-1 à 1
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n"); // passer à la ligne suivante
+    }
+
+    printf("La génération de la pyramide est terminée.\n");
+
+    return 0;
+}
