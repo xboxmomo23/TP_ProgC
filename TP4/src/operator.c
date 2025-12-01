060da50120ad800a81040cef96f15d7d2ef3204f
Nadir <94380694+xboxmomo23@users.noreply.github.com>
2025-12-01 10:10:29 +0100

    Update operator.c

---
+++ TP4/src/operator.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "Operator.h"
+
+int somme(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int difference(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int produit(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int quotient(int num1, int num2)
+{
+    if (num2 == 0)
+    {
+        printf("Erreur : division par zero.\n");
+        return 0;
+    }
+    return num1 / num2;
+}
+
+int modulo(int num1, int num2)
+{
+    if (num2 == 0)
+    {
+        printf("Erreur : modulo par zero.\n");
+        return 0;
+    }
+    return num1 % num2;
+}
+
+int et(int num1, int num2)
+{
+    return num1 & num2;
+}
+
+int ou(int num1, int num2)
+{
+    return num1 | num2;
+}
+
+int negation(int num1, int num2)
+{
+    (void)num2; /* on ignore le second paramètre */
+    return ~num1;
+}
