f97e60ead6c1f77e8cdd20eeaffea69fc940b8a0
Nadir <94380694+xboxmomo23@users.noreply.github.com>
2025-12-01 10:10:47 +0100

    Add header file for file operations

---
+++ TP4/src/fichier.h
@@ -0,0 +1,7 @@
+#ifndef FICHIER_H
+#define FICHIER_H
+
+void lire_fichier(const char *nom_de_fichier);
+void ecrire_dans_fichier(const char *nom_de_fichier, const char *message);
+
+#endif /* FICHIER_H */
